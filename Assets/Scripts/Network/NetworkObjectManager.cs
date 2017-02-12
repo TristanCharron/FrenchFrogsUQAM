69a1218c66177fd159a885916a889b76d820456c
Samuel <samuel.charron@live.ca>
2017-02-11 22:01:19 -0500

    Reviewer: Sam Bisou

---
+++ Assets/Scripts/Network/NetworkObjectManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkObjectManager : NetworkBehaviour {
+
+   [Command]
+    public void CmdTransferRocket(float x, float rotX, float rotY, float speed, int currentSprite)
+    {
+		Camera.main.GetComponent<UpperRocketManager> ().SpawnRocket (x, new Vector2 (rotX, rotY), speed, currentSprite);
+    }
+}
